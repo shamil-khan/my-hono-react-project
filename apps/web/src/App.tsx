02b988d060ae282f57fd6dfdf039b4c34b66297f
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 02:57:18 +0500

    Nice

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello: Frontend + Backend</h1>
+      <h1>Hello: Frontend + Backend + Nice</h1>
       <p>Message from Hono-Api: <strong>{message}</strong></p>
     </div>
   )
