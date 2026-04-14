8cb55a9d28efcf31654b6f7ea3a8165c821311fe
shamil-khan <shamil-ahmed@gmail.com>
2026-04-14 21:06:29 +0500

    fix(infra): use deploy

--- terraform/workers.tf
+++ terraform/workers.tf
@@ -1,4 +1,4 @@
-resource "cloudflare_worker_script" "api" {
+resource "cloudflare_workers_script" "api" {
   account_id = var.cloudflare_account_id
   name       = "my-hono-api"
   content    = file("${path.module}/../apps/api/dist/index.js")
@@ -8,11 +8,4 @@ resource "cloudflare_worker_script" "api" {
     name = "ALLOWED_ORIGIN"
     text = "https://my-hono-react-web.pages.dev"
   }
-}
-
-resource "cloudflare_worker_domain" "api_subdomain" {
-  account_id  = var.cloudflare_account_id
-  hostname    = "my-hono-api.shamil-khan.workers.dev"
-  service     = cloudflare_worker_script.api.name
-  environment = "production"
 }
