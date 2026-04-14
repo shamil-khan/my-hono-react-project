fb1c65e0dbe712b550a258a41f1e74e92dd90eea
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 04:48:28 +0500

    canteen

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello: Frontend + Backend + Shop</h1>
+      <h1>Hello: Frontend + Backend + Canteen</h1>
       <p>Message from Hono-Api: <strong>{message}</strong></p>
     </div>
   )
