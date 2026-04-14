fb9017288485fdc08a59c8772582c46e09411f3b
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 04:50:25 +0500

    soda

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello: Frontend + Backend + Canteen</h1>
+      <h1>Hello: Frontend + Backend + Soda</h1>
       <p>Message from Hono-Api: <strong>{message}</strong></p>
     </div>
   )
