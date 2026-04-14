3cb73322ed1512ae3be7b7b44ab0f23e6865511f
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 03:59:51 +0500

    rocket

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello: Frontend + Backend + Cool</h1>
+      <h1>Hello: Frontend + Backend + Rocket</h1>
       <p>Message from Hono-Api: <strong>{message}</strong></p>
     </div>
   )
