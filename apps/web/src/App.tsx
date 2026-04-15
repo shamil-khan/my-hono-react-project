5d5d3d7157b7b34843d114a4fcc81d071e5fc9a9
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 07:22:45 +0500

    hole

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello: Frontend + Backend + Boom</h1>
+      <h1>Hello: Frontend + Backend + Hole</h1>
       <p>Message from Hono-Api: <strong>{message}</strong></p>
     </div>
   )
