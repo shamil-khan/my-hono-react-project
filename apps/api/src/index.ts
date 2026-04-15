a964fc64049c805bf70102300525fa0c65538b0a
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 06:24:06 +0500

    cat

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -15,6 +15,6 @@ app.use('/*', async (c, next) => {
   return corsMiddleware(c, next)
 })
 
-app.get('/', (c) => c.text(`Hi Tiger! ${new Date().toLocaleString()}`))
+app.get('/', (c) => c.text(`Hi Cat! ${new Date().toLocaleString()}`))
 
 export default app
