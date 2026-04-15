fe4d75439fc5e0e2a709b213733bd0af211e3f92
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 05:03:23 +0500

    lime

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello: Frontend + Backend + Soda</h1>
+      <h1>Hello: Frontend + Backend + Lime</h1>
       <p>Message from Hono-Api: <strong>{message}</strong></p>
     </div>
   )
