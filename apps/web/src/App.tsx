ff0c9b4e1777fd9569190b083db6d60f97e572d9
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 05:08:32 +0500

    cola

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello: Frontend + Backend + Lime</h1>
+      <h1>Hello: Frontend + Backend + Cola</h1>
       <p>Message from Hono-Api: <strong>{message}</strong></p>
     </div>
   )
