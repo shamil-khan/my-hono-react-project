8692af90a8448276b5f4e5cd90daaf2f3a2f41fd
shamil-khan <shamil-ahmed@gmail.com>
2026-04-15 07:11:25 +0500

    kite

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -15,6 +15,6 @@ app.use('/*', async (c, next) => {
   return corsMiddleware(c, next)
 })
 
-app.get('/', (c) => c.text(`Hi Mouse! ${new Date().toLocaleString()}`))
+app.get('/', (c) => c.text(`Hi Kite! ${new Date().toLocaleString()}`))
 
 export default app
